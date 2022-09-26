45ed47535febdc520823cfee0d41ee2570dcf2e4
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2022-09-25 21:59:17 -0500

    Implement Pokedex List viewmodel and add the loading view UI
    - Add the viewmodel implementation to get the pokedex list, into the pokedex list fragment view
    - Add the lottie animation file to be used as a loading view in the project
    - Add the loading view to the project to be used across the fragments where data will be loaded
    - Use the loading animation views in the loading state of the pokedex list

--- app/src/main/java/com/sandoval/mypokedex/ui/pokedex_list/fragments/PokedexListFragment.kt
+++ app/src/main/java/com/sandoval/mypokedex/ui/pokedex_list/fragments/PokedexListFragment.kt
@@ -1,17 +1,22 @@
 package com.sandoval.mypokedex.ui.pokedex_list.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.sandoval.mypokedex.R
 import com.sandoval.mypokedex.databinding.FragmentPokedexListBinding
+import com.sandoval.mypokedex.ui.pokedex_list.viewmodel.GetPokedexListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PokedexListFragment : Fragment() {
+
+    private val getPokedexListViewModel: GetPokedexListViewModel by viewModels()
     private var _fragmentPokedexListBinding: FragmentPokedexListBinding? = null
     private val fragmentPokedexListBinding get() = _fragmentPokedexListBinding!!
 
@@ -24,6 +29,35 @@ class PokedexListFragment : Fragment() {
             findNavController().navigate(R.id.action_navigation_pokedex_list_fragment_to_navigation_pokedex_detail_fragment)
         }
 
+        getPokedexListViewModel.pokedexListViewModel.observe(viewLifecycleOwner) {
+            when {
+                it.loading -> {
+                    showLoading()
+                    Log.d("PokedexList", "Loading...")
+                }
+                it.isEmpty -> {
+                    hideLoading()
+                    Log.d("PokedexList Empty", "Empty...")
+                }
+                it.pokedexList != null -> {
+                    hideLoading()
+                    Log.d("PokedexList", it.pokedexList.toString())
+                }
+                it.errorMessage != null -> {
+                    hideLoading()
+                    Log.e("PokedexList Error", it.errorMessage.toString())
+                }
+            }
+        }
+
         return fragmentPokedexListBinding.root
+    }
+
+    private fun showLoading() {
+        fragmentPokedexListBinding.loadingSpinner.loadingContainer.visibility = View.VISIBLE
+    }
+
+    private fun hideLoading() {
+        fragmentPokedexListBinding.loadingSpinner.loadingContainer.visibility = View.GONE
     }
 }
