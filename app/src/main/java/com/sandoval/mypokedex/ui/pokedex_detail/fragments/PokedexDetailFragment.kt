f3d445767a5979acc1fd497e78c2efdb9a281880
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2022-09-28 13:25:32 -0500

    Add Abilities Types and Moves Lists
    - Add adapter for abilities
    - Add adapter for types
    - Add adapter for moves
    - Render the abilities list, the types list and the moves list in the pokedex detail fragment

--- app/src/main/java/com/sandoval/mypokedex/ui/pokedex_detail/fragments/PokedexDetailFragment.kt
+++ app/src/main/java/com/sandoval/mypokedex/ui/pokedex_detail/fragments/PokedexDetailFragment.kt
@@ -2,7 +2,6 @@ package com.sandoval.mypokedex.ui.pokedex_detail.fragments
 
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -10,6 +9,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.palette.graphics.Palette
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -20,9 +20,11 @@ import com.sandoval.mypokedex.R
 import com.sandoval.mypokedex.databinding.FragmentPokedexDetailBinding
 import com.sandoval.mypokedex.domain.models.pokedex_detail.DPokedexDetailResponse
 import com.sandoval.mypokedex.ui.base.BaseFragment
+import com.sandoval.mypokedex.ui.pokedex_detail.adapters.AbilitiesAdapter
+import com.sandoval.mypokedex.ui.pokedex_detail.adapters.MovesAdapter
+import com.sandoval.mypokedex.ui.pokedex_detail.adapters.TypesAdapter
 import com.sandoval.mypokedex.ui.pokedex_detail.viewmodel.GetPokedexDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
-
 
 @AndroidEntryPoint
 class PokedexDetailFragment : BaseFragment<FragmentPokedexDetailBinding>(
@@ -30,12 +32,47 @@ class PokedexDetailFragment : BaseFragment<FragmentPokedexDetailBinding>(
 ) {
 
     private val getPokedexDetailViewModel: GetPokedexDetailViewModel by viewModels()
+    private lateinit var abilitiesAdapter: AbilitiesAdapter
+    private lateinit var typesAdapter: TypesAdapter
+    private lateinit var movesAdapter: MovesAdapter
 
     private val args by navArgs<PokedexDetailFragmentArgs>()
     private var pokedexName: String? = null
     override fun initViews() {
         pokedexName = args.pokedexName
         (activity as AppCompatActivity?)!!.supportActionBar!!.title = pokedexName
+
+        abilitiesAdapter = AbilitiesAdapter()
+        typesAdapter = TypesAdapter()
+        movesAdapter = MovesAdapter()
+
+        abilitiesAdapter.apply {
+            registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+                override fun onItemRangeChanged(positionStart: Int, itemCount: Int) {
+                    binding.abilitiesList.smoothScrollToPosition(0)
+                }
+            })
+        }
+
+        typesAdapter.apply {
+            registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+                override fun onItemRangeChanged(positionStart: Int, itemCount: Int) {
+                    binding.typesList.smoothScrollToPosition(0)
+                }
+            })
+        }
+
+        movesAdapter.apply {
+            registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+                override fun onItemRangeChanged(positionStart: Int, itemCount: Int) {
+                    binding.movesList.smoothScrollToPosition(0)
+                }
+            })
+        }
+
+        setupAbilitiesRecycler()
+        setupTypesRecycler()
+        setupMovesRecycler()
     }
 
     override fun initViewModels() {
@@ -51,13 +88,36 @@ class PokedexDetailFragment : BaseFragment<FragmentPokedexDetailBinding>(
                 it.pokedexDetail != null -> {
                     hideLoading()
                     loadImage(it.pokedexDetail)
-                    binding.pokemonItemTitle.text = it.pokedexDetail.name
-                    Log.d("PokedexDetail", it.pokedexDetail.toString())
+                    val name = it.pokedexDetail.name.toString().replaceFirstChar { it.uppercase() }
+                    binding.pokemonItemTitle.text = name
                 }
                 it.errorMessage != null -> {
                     hideLoading()
-                    Log.e("PokedexList Error", it.errorMessage.toString())
                 }
+            }
+        }
+
+        getPokedexDetailViewModel.listAbilities.observe(viewLifecycleOwner) { listAbilities ->
+            if (listAbilities != null) {
+                abilitiesAdapter.submitPokedexAbilities(listAbilities)
+            } else {
+                binding.abilitiesList.visibility = View.GONE
+            }
+        }
+
+        getPokedexDetailViewModel.listMoves.observe(viewLifecycleOwner) { listMoves ->
+            if (listMoves != null) {
+                movesAdapter.submitPokedexMoves(listMoves)
+            } else {
+                binding.movesList.visibility = View.GONE
+            }
+        }
+
+        getPokedexDetailViewModel.listTypes.observe(viewLifecycleOwner) { listTypes ->
+            if (listTypes != null) {
+                typesAdapter.submitPokedexTypes(listTypes)
+            } else {
+                binding.typesList.visibility = View.GONE
             }
         }
     }
@@ -123,6 +183,21 @@ class PokedexDetailFragment : BaseFragment<FragmentPokedexDetailBinding>(
                 .into(pokemonItemImage)
 
         }
+    }
+
+    private fun setupAbilitiesRecycler() {
+        binding.abilitiesList.visibility = View.VISIBLE
+        binding.abilitiesList.adapter = abilitiesAdapter
+    }
+
+    private fun setupTypesRecycler() {
+        binding.typesList.visibility = View.VISIBLE
+        binding.typesList.adapter = typesAdapter
+    }
+
+    private fun setupMovesRecycler() {
+        binding.movesList.visibility = View.VISIBLE
+        binding.movesList.adapter = movesAdapter
     }
 
 }
--- app/src/main/java/com/sandoval/mypokedex/ui/pokedex_detail/viewmodel/GetPokedexDetailViewModel.kt
+++ app/src/main/java/com/sandoval/mypokedex/ui/pokedex_detail/viewmodel/GetPokedexDetailViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.sandoval.mypokedex.data.network.Failure
+import com.sandoval.mypokedex.domain.models.pokedex_detail.DAbilites
+import com.sandoval.mypokedex.domain.models.pokedex_detail.DMoves
 import com.sandoval.mypokedex.domain.models.pokedex_detail.DPokedexDetailResponse
+import com.sandoval.mypokedex.domain.models.pokedex_detail.DTypes
 import com.sandoval.mypokedex.domain.usecase.pokedex_detail.GetPokedexDetailUseCase
 import com.sandoval.mypokedex.ui.pokedex_detail.model.state.GetPokedexDetailView
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,6 +23,15 @@ class GetPokedexDetailViewModel @Inject constructor(private val getPokedexDetail
     private val _pokedexDetailViewModel = MutableLiveData<GetPokedexDetailView>()
     val pokedexDetailViewModel: LiveData<GetPokedexDetailView> get() = _pokedexDetailViewModel
 
+    private val _listAbilities = MutableLiveData<List<DAbilites>?>()
+    val listAbilities: LiveData<List<DAbilites>?> get() = _listAbilities
+
+    private val _listMoves = MutableLiveData<List<DMoves>?>()
+    val listMoves: LiveData<List<DMoves>?> get() = _listMoves
+
+    private val _listTypes = MutableLiveData<List<DTypes>?>()
+    val listTypes: LiveData<List<DTypes>?> get() = _listTypes
+
     fun getResults(name: String) {
         _pokedexDetailViewModel.value = GetPokedexDetailView(loading = true)
         getPokedexDetailUseCase(job, name) {
@@ -33,6 +45,10 @@ class GetPokedexDetailViewModel @Inject constructor(private val getPokedexDetail
     private fun handleSuccess(pokedexDetail: DPokedexDetailResponse) {
         _pokedexDetailViewModel.value = GetPokedexDetailView(loading = false)
         _pokedexDetailViewModel.value = GetPokedexDetailView(pokedexDetail = pokedexDetail)
+        _listAbilities.value = pokedexDetail.abilities
+        _listMoves.value = pokedexDetail.moves
+        _listTypes.value = pokedexDetail.types
+
     }
 
     private fun handleFailure(failure: Failure) {
